6732e14ac61e2175a6bfb77e367867324116dcb5
nPaldino <nPaldino@NPALDINO>
2017-02-21 16:23:15 -0500

    Initial commit.

---
+++ Ofl.Data.DependencyInjection.Autofac/Ofl.Data.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Autofac;
+using Ofl.Data.EntityFramework;
+using Ofl.Data.EntityFramework.Contracts;
+using Ofl.Data.SqlClient;
+using Ofl.Data.SqlClient.Contracts;
+
+namespace Ofl.Data.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        #region Overrides of Module
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Bind.
+            builder.RegisterType<SqlConnectionFactory>().As<ISqlConnectionFactory>();
+            builder.RegisterType<DbContextFactory>().As<IDbContextFactory>();
+        }
+
+        #endregion
+    }
+}
